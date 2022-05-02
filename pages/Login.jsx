a8b0b92ad59fe62833b3aec066aef379085bf2e7
Thomas Regourd <tom.reg@hotmail.fr>
2022-05-02 10:14:29 +0200

    add login page with inputs

---
+++ pages/Login.jsx
@@ -0,0 +1,55 @@
+import { Button, Input } from "@rneui/base";
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Login() {
+  const [username, setUserName] = useState();
+  const [userInput, setUserInput] = useState();
+
+  const onPressLogin = () => {
+    console.log(userInput);
+    setUserName(userInput);
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* <Text style={styles.title}>Login</Text> */}
+
+      <Input
+        style={styles.input}
+        placeholder="Username"
+        //leftIcon={{ type: "font-awesome", name: "chevron-left" }}
+        onChangeText={setUserInput}
+        value={userInput}
+      />
+      <Input
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+      />
+      <Button
+        title="Login"
+        type="solid"
+        icon={{ type: "font-awesome", name: "user", color: "white" }}
+        onPress={onPressLogin}
+      ></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 50,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  input: {
+    flex: 1,
+    textAlign: "center",
+  },
+});
